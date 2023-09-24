4bc932f172aa4ad6ca4b2fab9d8d91a5b2454f79
waiyon <ghostsketon@gmail.com>
2023-09-24 14:45:39 +0800

    Add(Page): Added landing page and profile page

---
+++ src/pages/landing.tsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export function LandingPage() {
+  return (
+    <Box bg={"blue.300"}>
+      <Heading>Landing Page</Heading>
+    </Box>
+  );
+}
---
+++ src/pages/profile.tsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export function ProfilePage() {
+  return (
+    <Box bg={"blue.300"}>
+      <Heading>Profile Page</Heading>
+    </Box>
+  );
+}
